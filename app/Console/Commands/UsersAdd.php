ee559190b99844a572e8f2defffe1927374608cd
darenweimer <darenweimer@gmail.com>
2021-03-31 15:17:51 -0700

    Added console command to add a user to the database

---
+++ app/Console/Commands/UsersAdd.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class UsersAdd extends Command
+{
+
+    /**
+     * The name and signature of the console command
+     *
+     * @var string
+     */
+    protected $signature = 'users:add
+        {name : The name of the user}
+        {email : The email address for the user account}
+        {password : The password for the user account}';
+
+    /**
+     * The console command description
+     *
+     * @var string
+     */
+    protected $description = 'Adds a new user to the database';
+
+    /**
+     * Executes the console command
+     *
+     * @return int
+     */
+    public function handle() : int
+    {
+        User::create([
+            'name'     => $this->argument('name'),
+            'email'    => $this->argument('email'),
+            'password' => Hash::make($this->argument('password')),
+        ]);
+
+        $this->info('The user was added to the database.');
+
+        return 0;
+    }
+
+}
