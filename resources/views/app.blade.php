ccd2c678e2750759e079e737c5bac381cc21dc7e
darenweimer <darenweimer@gmail.com>
2021-04-01 21:22:26 -0700

    Updated app master view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,26 +5,24 @@
 
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
     <title>
         {{ config('app.name') }}
     </title>
 
-    <link href="https://fonts.gstatic.com" rel="preconnect" />
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
+    <link href="https://fonts.gstatic.com" rel="preconnect"/>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"/>
 
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet" />
-
-    @yield('stylesheets')
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet"/>
 
     <script src="{{ mix('js/app.js') }}" defer></script>
 </head>
 
 
 
-<body class="font-sans font-normal text-base">
+<body class="font-sans font-light text-base">
     @routes
     @inertia
 </body>
