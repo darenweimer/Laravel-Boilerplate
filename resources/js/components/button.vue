6c2bb3fa9d2d262094380e8983adbd59ccc27feb
darenweimer <darenweimer@gmail.com>
2021-04-02 10:22:27 -0700

    Updated vue button component

--- resources/js/components/button.vue
+++ resources/js/components/button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :type="btnType" class="border focus:outline-none font-normal px-3 py-1 transition disabled:opacity-25" :class="btnClasses">
+    <button class="border rounded font-normal text-sm px-3 py-1 transition focus:outline-none disabled:opacity-25" :class="getExtClasses(action)">
         <slot/>
     </button>
 </template>
@@ -11,29 +11,15 @@
                 type: String,
                 default: 'submit',
             },
-            small: {
-                type: Boolean,
-                default: false,
-            },
-            round: {
-                type: Boolean,
-                default: false,
-            },
         },
 
-        data() {
-            return {
-                btnType:
-                    (this.action === 'submit') ? 'submit' : 'button',
-                btnClasses:
-                    ' border-button-' + this.action +
-                    ' bg-button-' + this.action +
-                    ' hover:bg-button-' + this.action + '-hover' +
-                    ' active:bg-button-' + this.action + '-active' +
-                    ' text-button-' + this.action +
-                    (this.small ? ' text-xs' : ' text-sm') +
-                    (this.round ? ' rounded-full' : ' rounded'),
-            };
+        methods: {
+            getExtClasses: (action) =>
+                ' border-button-' + action +
+                ' bg-button-' + action +
+                ' hover:bg-button-' + action + '-hover' +
+                ' active:bg-button-' + action + '-active' +
+                ' text-button-' + action,
         },
     };
 </script>
