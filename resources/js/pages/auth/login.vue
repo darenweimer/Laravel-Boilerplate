ab8aec60b9b7986f7f09e23e308550268af8cec1
darenweimer <darenweimer@gmail.com>
2021-04-12 20:13:52 -0700

    Added login page

---
+++ resources/js/pages/auth/login.vue
@@ -0,0 +1,64 @@
+<template>
+    <form @submit.prevent="submit">
+        <div class="flex h-screen content-center items-center">
+            <v-card class="w-full max-w-sm m-auto">
+                <template v-slot:header>
+                    <div class="text-center">
+                        Application Login
+                    </div>
+                </template>
+
+                <div class="my-4">
+                    <v-label label="Email Address"/>
+                    <v-input type="email" v-model="form.email" :error="form.errors.email" placeholder="Enter email address..." required autofocus/>
+                    <v-label :error="form.errors.email" :below="true"/>
+                </div>
+
+                <div class="my-4">
+                    <v-label label="Password"/>
+                    <v-input type="password" v-model="form.password" :error="form.errors.password" placeholder="Enter password..." required/>
+                    <v-label :error="form.errors.password" :below="true"/>
+                </div>
+
+                <div class="mt-6 mb-4">
+                    <v-checkbox label="Remember Me" :center="true" v-model="form.remember"/>
+                </div>
+
+                <template v-slot:footer>
+                    <div class="text-center">
+                        <v-button type="submit">
+                            Authenticate
+                        </v-button>
+
+                        <v-link :href="route('password.request')" class="ml-4 text-sm">
+                            Forgot password?
+                        </v-link>
+                    </div>
+                </template>
+            </v-card>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                form: this.useForm({
+                    email: null,
+                    password: null,
+                    remember: false,
+                }),
+            };
+        },
+        methods: {
+            submit () {
+                this.form
+                    .clearErrors()
+                    .post(this.route('login'), {
+                        onFinish: () => this.form.reset('password'),
+                    });
+            },
+        },
+    };
+</script>
