26b33008f2e2abe82c9a27929b039cec590663ab
darenweimer <darenweimer@gmail.com>
2021-04-12 19:33:17 -0700

    Added centering to checkbox component

--- resources/js/components/checkbox.vue
+++ resources/js/components/checkbox.vue
@@ -1,10 +1,10 @@
 <template>
     <label class="flex flex-row items-center">
-        <input type="checkbox" class="h-4 w-4 mr-2 appearance-none border border-input rounded-sm shadow-sm bg-input focus:outline-none focus:border-input-focus focus:ring focus:ring-input disabled:bg-input-disabled" :value="modelValue" v-model="proxyChecked" :disabled="disabled"/>
+        <input type="checkbox" class="h-4 w-4 mr-2 appearance-none border border-input rounded-sm shadow-sm bg-input focus:outline-none focus:border-input-focus focus:ring focus:ring-input disabled:bg-input-disabled" :class="center ? 'ml-auto' : null" :value="modelValue" v-model="proxyChecked" :disabled="disabled"/>
 
         <i v-if="modelValue" class="absolute ml-0.5 mb-0.5 fas fa-check text-input" :class="disabled ? 'text-input-disabled' : null"></i>
 
-        <span class="font-medium text-sm">
+        <span class="font-medium text-sm" :class="center ? 'mr-auto' : null">
             {{ label }}
         </span>
     </label>
@@ -13,7 +13,11 @@
 <script>
     export default {
         props: {
-            modelValue: {
+            label: {
+                type: String,
+                default: null,
+            },
+            center: {
                 type: Boolean,
                 default: false,
             },
@@ -21,7 +25,10 @@
                 type: Boolean,
                 default: false,
             },
-            label: String,
+            modelValue: {
+                type: Boolean,
+                default: false,
+            },
         },
         emits: [
             'update:modelValue',
