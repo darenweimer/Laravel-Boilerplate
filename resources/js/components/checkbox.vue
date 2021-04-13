3c88e083201ede6daae712a0cad431a119ead046
darenweimer <darenweimer@gmail.com>
2021-04-12 20:10:37 -0700

    Added proper centering to checkbox component

--- resources/js/components/checkbox.vue
+++ resources/js/components/checkbox.vue
@@ -1,13 +1,15 @@
 <template>
-    <label class="flex flex-row items-center">
-        <input type="checkbox" class="h-4 w-4 mr-2 appearance-none border border-input rounded-sm shadow-sm bg-input focus:outline-none focus:border-input-focus focus:ring focus:ring-input disabled:bg-input-disabled" :value="modelValue" v-model="proxyChecked" :disabled="disabled"/>
+    <div :class="center ? 'text-center' : null">
+        <label class="flex flex-row items-center" :class="center ? 'inline-flex' : null">
+            <input type="checkbox" class="h-4 w-4 mr-2 appearance-none border border-input rounded-sm shadow-sm bg-input focus:outline-none focus:border-input-focus focus:ring focus:ring-input disabled:bg-input-disabled" :value="modelValue" v-model="proxyChecked" :disabled="disabled"/>
 
-        <i v-if="modelValue" class="absolute ml-0.5 mb-0.5 fas fa-check text-input" :class="disabled ? 'text-input-disabled' : null"></i>
+            <i v-if="modelValue" class="absolute ml-0.5 mb-0.5 fas fa-check text-input" :class="disabled ? 'text-input-disabled' : null"></i>
 
-        <span class="font-medium text-sm">
-            {{ label }}
-        </span>
-    </label>
+            <span class="font-medium text-sm">
+                {{ label }}
+            </span>
+        </label>
+    </div>
 </template>
 
 <script>
