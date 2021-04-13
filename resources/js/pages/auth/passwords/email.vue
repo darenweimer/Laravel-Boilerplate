aeaff154d201afe8b92d8e8fb59ee568b41124e8
darenweimer <darenweimer@gmail.com>
2021-04-12 20:53:53 -0700

    Added reset password email page

---
+++ resources/js/pages/auth/passwords/email.vue
@@ -0,0 +1,46 @@
+<template>
+    <form @submit.prevent="submit">
+        <div class="flex h-screen content-center items-center">
+            <v-card class="w-full max-w-sm m-auto">
+                <template v-slot:header>
+                    <div class="text-center">
+                        Reset Password
+                    </div>
+                </template>
+
+                <div class="my-4">
+                    <v-label label="Email Address"/>
+                    <v-input type="email" v-model="form.email" :error="form.errors.email" placeholder="Enter email address..." required autofocus/>
+                    <v-label :success="status" :error="form.errors.email" :below="true"/>
+                </div>
+
+                <template v-slot:footer>
+                    <div class="text-center">
+                        <v-button type="submit" :disabled="form.processing">
+                            Send Password Reset Link
+                        </v-button>
+                    </div>
+                </template>
+            </v-card>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                form: this.useForm({
+                    email: this.user ? this.user.email : null,
+                }),
+            };
+        },
+        methods: {
+            submit () {
+                this.form
+                    .clearErrors()
+                    .post(this.route('password.email'));
+            },
+        },
+    };
+</script>
