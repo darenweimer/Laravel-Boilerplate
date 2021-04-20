5e01dc14ad5b426592a0dd34e2c6bc7a64662264
darenweimer <darenweimer@gmail.com>
2021-04-19 18:59:08 -0700

    Updated auth web routes

--- routes/web/auth.php
+++ routes/web/auth.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,17 +16,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::namespace('Auth')->group(function () {
 
-    Route::get('login', 'LoginController@loginForm')->name('login');
-    Route::post('login', 'LoginController@login');
-    Route::get('logout', 'LoginController@logout')->name('logout');
+    Route::get('login',  [LoginController::class, 'loginForm'])->name('login');
+    Route::post('login', [LoginController::class, 'login'    ]);
+    Route::get('logout', [LoginController::class, 'logout'   ])->name('logout');
 
     Route::prefix('password')->name('password.')->group(function () {
 
-        Route::get('reset', 'ForgotPasswordController@linkRequestForm')->name('request');
-        Route::post('email', 'ForgotPasswordController@sendResetLinkEmail')->name('email');
+        Route::get('reset',  [ForgotPasswordController::class, 'linkRequestForm'   ])->name('request');
+        Route::post('email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('email');
 
-        Route::get('reset/{token}', 'ResetPasswordController@resetForm')->name('reset');
-        Route::post('reset', 'ResetPasswordController@reset')->name('update');
+        Route::get('reset/{token}', [ResetPasswordController::class, 'resetForm'])->name('reset');
+        Route::post('reset',        [ResetPasswordController::class, 'reset'    ])->name('update');
 
     });
 
