25285b52b2017f4cd7dd88c2d7f7efd49571f887
darenweimer <darenweimer@gmail.com>
2021-04-24 14:37:39 -0700

    Changed text size on label component

--- resources/js/components/label.vue
+++ resources/js/components/label.vue
@@ -4,11 +4,11 @@
             {{ label }}
         </label>
 
-        <label v-if="success" class="block w-full font-normal text-xs text-input-success" :class="below ? 'mt-1' : 'mb-1'">
+        <label v-if="success" class="block w-full font-normal text-sm text-input-success" :class="below ? 'mt-1' : 'mb-1'">
             {{ success }}
         </label>
 
-        <label v-if="error" class="block w-full font-normal text-xs text-input-error" :class="below ? 'mt-1' : 'mb-1'">
+        <label v-if="error" class="block w-full font-normal text-sm text-input-error" :class="below ? 'mt-1' : 'mb-1'">
             {{ error }}
         </label>
     </div>
