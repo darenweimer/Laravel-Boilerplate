5c871e32c4fb666add9a3d11d66113d3b069bf43
darenweimer <darenweimer@gmail.com>
2021-04-24 14:37:18 -0700

    Removed v-link from dropdown

--- resources/js/components/dropdown.vue
+++ resources/js/components/dropdown.vue
@@ -9,7 +9,7 @@
         <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-75" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-100" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-75">
             <div v-if="menuActive" class="absolute inline right-0 top-full z-50 border border-dropdown rounded bg-dropdown text-left whitespace-nowrap py-1">
                 <ul>
-                    <v-link v-for="(item, text) in menu" :href="item.link" :key="text">
+                    <inertia-link v-for="(item, text) in menu" :href="item.link" :key="text">
                         <li class="text-dropdown pl-4 pr-12 py-1 hover:bg-dropdown-highlight hover:text-dropdown-highlight">
                             <i v-if="item.icon" class="mr-3 fa-fw text-xl align-middle opacity-50" :class="item.icon"/>
 
@@ -17,7 +17,7 @@
                                 {{ text }}
                             </span>
                         </li>
-                    </v-link>
+                    </inertia-link>
                 </ul>
             </div>
         </transition>
