06bac14eabb5664c3fa08b0c8b3ab9b263264465
darenweimer <darenweimer@gmail.com>
2021-08-03 16:08:31 -0700

    Updated spacing on card component

--- resources/js/components/card.vue
+++ resources/js/components/card.vue
@@ -1,14 +1,14 @@
 <template>
     <div :class="`border border-card-${color} rounded-lg font-normal text-base p-0 overflow-hidden`">
-        <div v-if="$slots.header" :class="`border-b border-card-${color} bg-card-${color}-header font-medium text-lg text-card-${color}-header px-4 py-3`">
+        <div v-if="$slots.header" :class="`border-b border-card-${color} bg-card-${color}-header font-medium text-xl text-card-${color}-header p-4`">
             <slot name="header"/>
         </div>
 
-        <div :class="`bg-card-${color}-body text-card-${color}-body px-4 py-3`">
+        <div :class="`bg-card-${color}-body text-card-${color}-body p-4`">
             <slot/>
         </div>
 
-        <div v-if="$slots.footer" :class="`border-t border-card-${color} bg-card-${color}-footer text-card-${color}-footer px-4 py-3`">
+        <div v-if="$slots.footer" :class="`border-t border-card-${color} bg-card-${color}-footer text-card-${color}-footer p-4`">
             <slot name="footer"/>
         </div>
     </div>
