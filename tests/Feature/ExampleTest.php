a96f95a4ff05f4a24cf764f4fc79f1e1b5e0d428
darenweimer <darenweimer@gmail.com>
2021-08-09 15:04:03 -0700

    Updates for latest Laravel version 8.x

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,7 +13,7 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function test_example()
     {
         $response = $this->get('/');
 
