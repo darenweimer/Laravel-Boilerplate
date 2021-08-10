56faffc9512d668dc2c76051940e467b84fb6521
darenweimer <darenweimer@gmail.com>
2021-08-09 18:13:39 -0700

    Removed unused class reference

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use App\Models\Traits\DateDisplay;
 use App\Models\Traits\Revisions;
 use App\Models\Traits\UserSettings;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
