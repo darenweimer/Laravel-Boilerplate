9409e37897c077c163e89d97c814659a410bf451
darenweimer <darenweimer@gmail.com>
2021-10-11 15:14:06 -0700

    Updated permissions script

--- permissions.sh
+++ permissions.sh
@@ -7,13 +7,13 @@ fi
 find . -type d -not -path "./node_modules/*" -not -path "./storage/*" -not -path "./vendor/*" -exec chmod 755 {} \;
 find . -type f -not -path "./node_modules/*" -not -path "./storage/*" -not -path "./vendor/*" -exec chmod 644 {} \;
 
-chmod 754 permissions
-chmod 754 deploy
-
 chmod -R 775 bootstrap/cache
 chmod -R 775 node_modules
 chmod -R 775 storage
 chmod -R 775 vendor
+
+chmod 754 permissions.sh
+chmod 754 deploy.sh
 
 if [ -d ".git" ]; then
     chmod -R 755 .git
