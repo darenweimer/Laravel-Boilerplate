06595e19d6251f835b554a7d14f68be80266b4f9
darenweimer <darenweimer@gmail.com>
2022-01-14 14:50:14 -0800

    Updated revisions trait for pivot tables

--- app/Models/Traits/Revisions.php
+++ app/Models/Traits/Revisions.php
@@ -3,10 +3,18 @@
 namespace App\Models\Traits;
 
 use App\Models\Revision;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 use Illuminate\Support\Facades\Auth;
 
 trait Revisions
 {
+
+    /**
+     * The primary key value for the model being deleted
+     *
+     * @var mixed
+     */
+    protected $revisionsDeleting = null;
 
     /**
      * Handles the model's 'created' event
@@ -15,25 +23,27 @@ trait Revisions
      */
     protected function revisionsAfterCreated()
     {
-        $exclusions = array_merge(
-            $this->hidden ?? [],
-            $this->dontRevision ?? [],
-            ['id', 'updated_at']
-        );
+        if ($id = $this->attributes[$this->primaryKey] ?? null) {
+            $exclusions = array_merge(
+                $this->hidden ?? [],
+                $this->dontRevision ?? [],
+                [$this->primaryKey, 'updated_at']
+            );
 
-        $attributes = array_diff_key(
-            $this->attributes, array_flip($exclusions)
-        );
+            $attributes = array_diff_key(
+                $this->attributes, array_flip($exclusions)
+            );
 
-        foreach ($attributes as $attribute => $value) {
-            Revision::create([
-                'revisionable_type' => static::class,
-                'revisionable_id'   => $this->attributes[$this->primaryKey],
-                'user_id'           => Auth::id(),
-                'key'               => $attribute,
-                'old_value'         => null,
-                'new_value'         => $value,
-            ]);
+            foreach ($attributes as $attribute => $value) {
+                Revision::create([
+                    'revisionable_type' => static::class,
+                    'revisionable_id'   => $id,
+                    'user_id'           => Auth::id(),
+                    'key'               => $attribute,
+                    'old_value'         => null,
+                    'new_value'         => $value,
+                ]);
+            }
         }
     }
 
@@ -67,20 +77,50 @@ trait Revisions
     }
 
     /**
+     * Handles the model's 'deleting' event
+     *
+     * @return void
+     */
+    protected function revisionsAfterDeleting()
+    {
+        $this->revisionsDeleting = $this->attributes[$this->primaryKey] ?? null;
+
+        if (isset($this->revisionsDeleting)) {
+            return;
+        }
+
+        if (!($this instanceOf \Illuminate\Database\Eloquent\Relations\Pivot)) {
+            return;
+        }
+
+        $matches = static::select($this->primaryKey)
+            ->where($this->attributes)
+            ->get();
+
+        if ($matches->count() === 1) {
+            $this->revisionsDeleting = $matches->first()->{$this->primaryKey};
+        }
+    }
+
+    /**
      * Handles the model's 'deleted' event
      *
      * @return void
      */
     protected function revisionsAfterDeleted()
     {
-        Revision::create([
-            'revisionable_type' => static::class,
-            'revisionable_id'   => $this->attributes[$this->primaryKey],
-            'user_id'           => Auth::id(),
-            'key'               => 'deleted_at',
-            'old_value'         => null,
-            'new_value'         => $this->attributes['deleted_at'] ?? now(),
-        ]);
+        if (isset($this->revisionsDeleting)) {
+            Revision::create([
+                'revisionable_type' => static::class,
+                'revisionable_id'   => $this->revisionsDeleting,
+                'user_id'           => Auth::id(),
+                'key'               => 'deleted_at',
+                'old_value'         => null,
+                'new_value'         => $this->attributes['deleted_at'] ?? now(),
+            ]);
+
+            $this->revisionsDeleting = null;
+        }
     }
 
     /**
@@ -96,6 +136,10 @@ trait Revisions
 
         static::updated(function ($model) {
             $model->revisionsAfterUpdated();
+        });
+
+        static::deleting(function ($model) {
+            $model->revisionsAfterDeleting();
         });
 
         static::deleted(function ($model) {
