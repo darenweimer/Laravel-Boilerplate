570efad36b180daa93e5a0cd02737ad364029ce2
darenweimer <darenweimer@gmail.com>
2022-01-18 06:38:48 -0800

    Updated theme page

--- resources/js/pages/theme.vue
+++ resources/js/pages/theme.vue
@@ -82,7 +82,7 @@
             <v-label success="This is an example success label" :below="true"/>
 
             <v-label label="Example Label" class="mt-4"/>
-            <v-input invalid="Formatted error" value="This is an example invalid input value" placeholder="This is an example text input..."/>
+            <v-input :invalid="true" value="This is an example invalid input value" placeholder="This is an example text input..."/>
             <v-label error="This is an example error label" :below="true"/>
         </div>
 
