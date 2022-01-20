b809b93ce20dd4539a42bb4df609a96a01e84a79
darenweimer <darenweimer@gmail.com>
2022-01-19 17:24:00 -0800

    Added model cast class for decimal support class

---
+++ app/Models/Casts/Decimal.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models\Casts;
+
+use App\Support\Decimal as DecimalCast;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+class Decimal implements CastsAttributes
+{
+
+    /**
+     * Casts the given value
+     *
+     * @param mixed $model
+     * @param string $key
+     * @param mixed $value
+     * @param array $attributes
+     *
+     * @return DecimalCast|null
+     */
+    public function get($model, string $key, $value, array $attributes) : ?DecimalCast
+    {
+        return isset($value) ? (new DecimalCast($value)) : null;
+    }
+
+    /**
+     * Prepares the given value for storage
+     *
+     * @param mixed $model
+     * @param string $key
+     * @param mixed $value
+     * @param array $attributes
+     *
+     * @return string|null
+     */
+    public function set($model, string $key, $value, array $attributes) : ?string
+    {
+        return isset($value) ? ((string) $value) : null;
+    }
+
+}
