65f9d6e3c2f008474cd2df23a71e2eadc293ffb3
darenweimer <darenweimer@gmail.com>
2022-03-17 13:23:59 -0700

    Added a user session expire console command

---
+++ app/Console/Commands/UserExpire.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class UserExpire extends Command
+{
+
+    /**
+     * The name and signature of the console command
+     *
+     * @var string
+     */
+    protected $signature = 'user:expire
+        {--id=* : The optional user ids to expire}';
+
+    /**
+     * The console command description
+     *
+     * @var string
+     */
+    protected $description = 'Expires user sessions';
+
+    /**
+     * Executes the console command
+     *
+     * @return int
+     */
+    public function handle() : int
+    {
+        $query = DB::table('sessions');
+
+        if ($users = $this->option('id')) {
+            $query->whereIn('user_id', $users);
+        }
+
+        $query->delete();
+
+        $this->info('All selected user sessions have been expired.');
+
+        return 0;
+    }
+
+}
