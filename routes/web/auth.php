34d15625aa5325e8c7a1f1309414a8d0deaeead6
darenweimer <darenweimer@gmail.com>
2022-03-24 13:13:17 -0700

    Removed laravel/ui composer package

--- routes/web/auth.php
+++ routes/web/auth.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\Auth\ForgotPasswordController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\Auth\TwoFactorController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,38 +9,12 @@ use Illuminate\Support\Facades\Route;
 |-------------------------------------------------------------------------------
 */
 
-Route::controller(LoginController::class)->group(function () {
-
-    Route::get('login',  'loginForm')->name('login');
-    Route::post('login', 'login'    );
-    Route::get('logout', 'logout'   )->name('logout');
-
-});
-
 Route::controller(TwoFactorController::class)->group(function () {
 
     Route::prefix('two-factor')->name('two-factor.')->group(function () {
 
         Route::get('verify',  'verifyForm')->name('verify');
         Route::post('verify', 'verify'    );
-
-    });
-
-});
-
-Route::prefix('password')->name('password.')->group(function () {
-
-    Route::controller(ForgotPasswordController::class)->group(function () {
-
-        Route::get('reset',  'linkRequestForm'   )->name('request');
-        Route::post('email', 'sendResetLinkEmail')->name('email');
-
-    });
-
-    Route::controller(ResetPasswordController::class)->group(function () {
-
-        Route::get('reset/{token}', 'resetForm')->name('reset');
-        Route::post('reset',        'reset'    )->name('update');
 
     });
 
