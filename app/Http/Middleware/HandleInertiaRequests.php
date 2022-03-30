88da688bc8f80c0897501729957e6b342fbe480d
darenweimer <darenweimer@gmail.com>
2022-03-30 16:34:47 -0700

    Added loading of missing user settings to myself inertia prop

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -45,6 +45,7 @@ class HandleInertiaRequests extends Middleware
         return array_merge(
             parent::share($request), [
                 'myself' => fn() => $request->user()
+                    ->loadMissing('userSettings')
                     ->withPermissions(),
                 'env'    => fn() => config('app.env'),
                 'status' => fn() => session('status'),
