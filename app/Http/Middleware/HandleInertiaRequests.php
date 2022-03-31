f562abd120b2387ccd3ed385890cb17da1533b28
darenweimer <darenweimer@gmail.com>
2022-03-30 17:33:24 -0700

    Fixed inertia null user on myself prop

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -45,7 +45,7 @@ class HandleInertiaRequests extends Middleware
         return array_merge(
             parent::share($request), [
                 'myself' => fn() => $request->user()
-                    ->loadMissing('userSettings')
+                    ?->loadMissing('userSettings')
                     ->withPermissions(),
                 'env'    => fn() => config('app.env'),
                 'status' => fn() => session('status'),
