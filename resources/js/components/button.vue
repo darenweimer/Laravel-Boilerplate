ceb158ca7322ef9a9a2642c8a7dc900e98196c83
darenweimer <darenweimer@gmail.com>
2022-04-11 15:44:51 -0700

    Removed normal from button and dropdown class names

--- resources/js/components/button.vue
+++ resources/js/components/button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :type="type" :class="`focus:outline-none focus:ring focus:ring-input rounded bg-btn-${color}-normal hover:bg-btn-${color}-hover active:bg-btn-${color}-active font-default font-bold text-sm text-btn-${color} tracking-wide uppercase px-2.5 py-1.5 disabled:opacity-40 transition`">
+    <button :type="type" :class="`focus:outline-none focus:ring focus:ring-input rounded bg-btn-${color} hover:bg-btn-${color}-hover active:bg-btn-${color}-active font-default font-bold text-sm text-btn-${color} tracking-wide uppercase px-2.5 py-1.5 disabled:opacity-40 transition`">
         <slot>
         </slot>
     </button>
--- resources/js/components/dropdown.vue
+++ resources/js/components/dropdown.vue
@@ -6,7 +6,7 @@
         </button>
 
         <transition :class="origin" enter-active-class="transition ease-out duration-150" enter-from-class="scale-0" enter-to-class="scale-100" leave-active-class="transition ease-in duration-150" leave-from-class="scale-100" leave-to-class="scale-0">
-            <div v-show="show" :class="`absolute inline z-[100] ${position} rounded shadow-md bg-dropdown-normal font-default font-normal text-base text-dropdown-normal tracking-wide text-left py-1 whitespace-nowrap`">
+            <div v-show="show" :class="`absolute inline z-[100] ${position} rounded shadow-md bg-dropdown font-default font-normal text-base text-dropdown tracking-wide text-left py-1 whitespace-nowrap`">
                 <ul>
                     <inertia-link v-for="(option, index) in options" :key="index" :href="option.href">
                         <li class="hover:bg-dropdown-highlight hover:text-dropdown-highlight py-1.5">
