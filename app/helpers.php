7709cffe6ea8345d83c7b39ca03c21718b12d47c
darenweimer <darenweimer@gmail.com>
2022-05-20 13:31:44 -0700

    Alphabetized helper functions

--- app/helpers.php
+++ app/helpers.php
@@ -12,43 +12,6 @@ use App\Models\Setting;
 */
 
 /**
- * Tracks how long a callback takes to execute a certain number of times
- *
- * This function also takes into account the time it takes to run a callback for
- * the same number of iterations and subtracts that time from the final time,
- * yielding a more accurate runtime for the callback itself.
- *
- * @param int $iterations
- * @param callable $callback
- *
- * @return float
- */
-function stopwatch(int $iterations, callable $callback) : float
-{
-    $emptyFunc = function () { };
-
-    $startTime = microtime(true);
-
-    for ($i = 0; $i < $iterations; $i++) {
-        $emptyFunc();
-    }
-
-    $endTime = microtime(true);
-
-    $delta = $endTime - $startTime;
-
-    $startTime = microtime(true);
-
-    for ($i = 0; $i < $iterations; $i++) {
-        $callback();
-    }
-
-    $endTime = microtime(true);
-
-    return max($endTime - $startTime - $delta, 0);
-}
-
-/**
  * Returns true if an array matches an items expression
  *
  * $items can be a single item or a combination of items linked by || or &&.
@@ -118,4 +81,41 @@ function put_setting(string $setting, mixed $value, int $expiresAt = null) : voi
         'value'      => $value,
         'expires_at' => $expiresAt,
     ]);
+}
+
+/**
+ * Tracks how long a callback takes to execute a certain number of times
+ *
+ * This function also takes into account the time it takes to run a callback for
+ * the same number of iterations and subtracts that time from the final time,
+ * yielding a more accurate runtime for the callback itself.
+ *
+ * @param int $iterations
+ * @param callable $callback
+ *
+ * @return float
+ */
+function stopwatch(int $iterations, callable $callback) : float
+{
+    $emptyFunc = function () { };
+
+    $startTime = microtime(true);
+
+    for ($i = 0; $i < $iterations; $i++) {
+        $emptyFunc();
+    }
+
+    $endTime = microtime(true);
+
+    $delta = $endTime - $startTime;
+
+    $startTime = microtime(true);
+
+    for ($i = 0; $i < $iterations; $i++) {
+        $callback();
+    }
+
+    $endTime = microtime(true);
+
+    return max($endTime - $startTime - $delta, 0);
 }
