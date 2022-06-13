e3f9bd0b67a9eb30d045878c6d59a56b1b599dc4
darenweimer <darenweimer@gmail.com>
2022-06-13 09:56:23 -0700

    Slowed down modal enter transition

--- resources/js/components/modal.vue
+++ resources/js/components/modal.vue
@@ -2,14 +2,14 @@
     <teleport to="body">
         <transition leave-active-class="duration-150">
             <div v-show="show" class="fixed inset-0 z-[100] px-4 py-12 sm:py-24 overflow-y-auto" scroll-region>
-                <transition enter-active-class="ease-out duration-150" enter-from-class="opacity-0" enter-to-class="opacity-100" leave-active-class="ease-in duration-150" leave-from-class="opacity-100" leave-to-class="opacity-0">
+                <transition enter-active-class="ease-out duration-300" enter-from-class="opacity-0" enter-to-class="opacity-100" leave-active-class="ease-in duration-150" leave-from-class="opacity-100" leave-to-class="opacity-0">
                     <div v-show="show" class="fixed inset-0 transform transition-all" @click="close()">
                         <div class="absolute inset-0 bg-black opacity-75">
                         </div>
                     </div>
                 </transition>
 
-                <transition enter-active-class="ease-out duration-150" enter-from-class="opacity-0 scale-50" enter-to-class="opacity-100 scale-100" leave-active-class="ease-in duration-150" leave-from-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-50">
+                <transition enter-active-class="ease-out duration-300" enter-from-class="opacity-0 scale-50" enter-to-class="opacity-100 scale-100" leave-active-class="ease-in duration-150" leave-from-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-50">
                     <div v-show="show" class="flex flex-col max-h-full mx-auto px-5 py-4 overflow-hidden rounded-lg shadow-[0_25px_50px_rgb(0,0,0)] bg-default font-default font-normal text-base text-default tracking-wide transform transition-all" :class="`max-w-${size}`">
                         <div v-if="$slots.header" class="flex-none flex flex-row items-center mb-4 pb-4 border-b border-default text-2xl">
                             <div class="grow font-bold text-title">
