0a9e4d8383b778810e7f2cabc2cdc02e1d75e9bc
darenweimer <darenweimer@gmail.com>
2022-06-13 08:55:05 -0700

    Added form group vue component

---
+++ resources/js/components/form-group.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="font-default font-normal text-base text-default tracking-wide" :class="{ 'contents': inline }">
+        <div v-if="$slots.label" class="mb-1" :class="{ 'mt-1.5': inline }">
+            <slot name="label">
+            </slot>
+        </div>
+
+        <div>
+            <div :class="{ 'mb-1': $slots.help || $slots.success || $slots.error }">
+                <slot>
+                </slot>
+            </div>
+
+            <div v-if="$slots.help" class="text-info text-sm">
+                <slot name="help">
+                </slot>
+            </div>
+
+            <div v-if="$slots.success" class="text-success text-sm">
+                <slot name="success">
+                </slot>
+            </div>
+
+            <div v-if="$slots.error" class="text-error text-sm">
+                <slot name="error">
+                </slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            inline: {
+                type: Boolean,
+                default: false,
+            },
+        },
+    };
+</script>
