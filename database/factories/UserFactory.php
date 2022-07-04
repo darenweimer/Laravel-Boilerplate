e8925200fd656c3e3181b89bb90e6b1ac1ebfff9
darenweimer <darenweimer@gmail.com>
2022-07-04 11:59:30 -0700

    Removed uniqueness from user factory name components

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,8 +16,8 @@ class UserFactory extends Factory
     public function definition() : array
     {
         return [
-            'first_name'        => fake()->unique()->firstName(),
-            'last_name'         => fake()->unique()->lastName(),
+            'first_name'        => fake()->firstName(),
+            'last_name'         => fake()->lastName(),
             'email'             => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
