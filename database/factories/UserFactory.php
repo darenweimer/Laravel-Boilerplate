fba1937b851a592dc5e5373ec45b8703a4d125c9
darenweimer <darenweimer@gmail.com>
2022-07-04 11:34:59 -0700

    Updated user factory to use proper fake data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,14 +15,10 @@ class UserFactory extends Factory
      */
     public function definition() : array
     {
-        $name = explode(
-            ' ', $this->faker->name()
-        );
-
         return [
-            'first_name'        => $name[0],
-            'last_name'         => $name[1],
-            'email'             => $this->faker->unique()->safeEmail(),
+            'first_name'        => fake()->unique()->firstName(),
+            'last_name'         => fake()->unique()->lastName(),
+            'email'             => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token'    => Str::random(10),
