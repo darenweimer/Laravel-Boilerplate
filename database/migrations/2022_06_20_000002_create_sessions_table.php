c3853e115abde8504e34b27095d5d3da91c33405
darenweimer <darenweimer@gmail.com>
2022-07-18 15:25:08 -0700

    Added auto increment primary key to sessions table for platforms that require it

--- database/migrations/2022_06_20_000002_create_sessions_table.php
+++ database/migrations/2022_06_20_000002_create_sessions_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     public function up() : void
     {
         Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
+            $table->id('auto_id');
+            $table->string('id')->unique();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
